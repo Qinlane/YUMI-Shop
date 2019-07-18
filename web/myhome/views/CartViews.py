eaeaddc0ef4fbdef084bd74a36644c743b871986
Qinlane <386362711@qq.com>
2019-07-18 22:11:37 +0800

    info 完善 购物车雏形

--- web/myhome/views/CartViews.py
+++ web/myhome/views/CartViews.py
@@ -3,12 +3,14 @@ from django.http import HttpResponse, Http404,JsonResponse
 
 from myadmin.models import Users,Cates,Goods,Cart
 
-
+# 购物车列表
 def myhome_cart_index(request):
-   
+    # 获取当前用户的购物车数据
+    VipUser = request.session.get('VipUser')
+    data = Cart.objects.filter(uid=VipUser['uid'])
+    context = {'cartdata':data}
 
-
-    return HttpResponse('myhome_cart_index')
+    return render(request,'myhome/cart/index.html',context)
 
 def myhome_cart_add(request):
     
@@ -20,11 +22,14 @@ def myhome_cart_add(request):
         # 先检测当前的商品是否已经在当前用户的购物车中
         ob = Cart.objects.filter(uid=data['uid']).filter(goodsid=data['goodsid'])
 
-        print(ob)
         if ob.count():
             # 存在 获取当前购物车对象
             cart = Cart.objects.get(id=ob[0].id)
-            cart.num += int(data['num'])
+            num=int(data['num'])
+            cart.num += num
+            for v in range(1,num+1):
+                cart.sizes+=(','+data['sizes'])
+                cart.color+=(','+data['color'])
             cart.save()
 
         else:
